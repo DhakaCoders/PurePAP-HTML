30c1baef9b94364ebd2bb58d445db74302011064
Foridul Islam <foridulsumon@gmail.com>
2020-03-12 20:57:24 +0600

    GU

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,30 +14,6 @@ if($('.tbody-td').length){
   $('.tbody-td, .tfooter-td, .tfooter-td').matchHeight();
 };
 
-//$('[data-toggle="tooltip"]').tooltip();
-
-
-$('.xs-order-now-btn a,.order-con-btn').on('click', function(e){
-  e.preventDefault();
-  $('body').addClass('xs-order-sec-cntlr');
-});
-
-$('.xs-back-btn a').on('click', function(e){
-  e.preventDefault();
-  $('body').removeClass('xs-order-sec-cntlr');
-});
-
-// body animate
-if($('.scroll').length){
-  $(".scroll").click(function(e) {
-      e.preventDefault();
-      var goto = $(this).attr('href');
-      $('html, body').animate({
-          scrollTop: $(goto).offset().top - 0
-      }, 800);
-  });
-
-}
 
 if($('.fancybox').length){
 $('.fancybox').fancybox({
@@ -58,10 +34,74 @@ $('.faq-accoordion-wrp').on('click', '.faq-accoordion-cp', function() {
     $(this).parent().siblings().find('span').removeClass('active');
     $(this).closest('.faq-accoordion').find('.ac-box-title').stop().slideToggle();
 });
-
+$(document).ready(function() {
+  if( windowWidth < 576 && $('#select-order-sec').length ){
+    goToByScroll(".select-offer-boxs-section", 0);
+  }
+});
 $('#differentShippingAddress').on('change', function(){
-  
+  if ($(this).is(':checked')) {
+    $('.dffshippingAddress').show();
+  }else{
+    $('.dffshippingAddress').hide();
+  }
 });
 
+
+// body animate
+if($('.scroll').length){
+  $(".scroll").click(function(e) {
+      e.preventDefault();
+      var goto = $(this).attr('href');
+      $('html, body').animate({
+          scrollTop: $(goto).offset().top - 0
+      }, 800);
+  });
+
+}
+
+if( $('.order-con-btn').length ){
+  $(".order-con-btn").click(function(e) {
+      e.preventDefault();
+      $('html, body').animate({
+          scrollTop: $('.orderCart').offset().top
+      }, 800);
+  });  
+}
+if( $('.xsOrdertoCart').length ){
+  $(".xsOrdertoCart").click(function(e) {
+      e.preventDefault();
+      $('html, body').animate({
+          scrollTop: $('.orderCart').offset().top
+      }, 800);
+  });  
+}
+if( $('.goToVideo').length ){
+  $(".goToVideo").click(function(e) {
+      e.preventDefault();
+      $('html, body').animate({
+          scrollTop: $('.save-time-section').offset().top
+      }, 800);
+  });  
+}
+if( $('.theDifference').length ){
+  $(".theDifference").click(function(e) {
+      e.preventDefault();
+      $('html, body').animate({
+          scrollTop: $('.competitors-section').offset().top
+      }, 800);
+  });  
+}
+
+function goToByScroll(id, offset){
+  if(id){
+      // Remove "link" from the ID
+    id = id.replace("link", "");
+      // Scroll
+    $('html,body').animate(
+        {scrollTop: $(id).offset().top - offset},
+      500);
+  }
+}
 
 })(jQuery);
